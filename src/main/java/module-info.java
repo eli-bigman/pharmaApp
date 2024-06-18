960156578c1f28aa6df6e58f30b9845792aff049
eli-bigman <nutsugah81@gmail.com>
2024-06-18 23:08:52 +0000

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.pharmaapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.pharmaapp to javafx.fxml;
+    exports com.example.pharmaapp;
+}
